27703b886d6f462dcf691c2dca74fd1d285a670a
Dan Wells <dantheradarman@gmail.com>
2022-05-26 20:55:10 +0100

    Unknown nodes now still store their text

--- server/src/analyzerv2/Unknown.ts
+++ server/src/analyzerv2/Unknown.ts
@@ -1,4 +1,5 @@
 import { Node, ts } from 'ts-morph';
+import { FrequencyList } from './FrequencyList';
 import { GenericNode } from './Generic';
 
 export class UnknownNode extends GenericNode {
@@ -6,19 +7,26 @@ export class UnknownNode extends GenericNode {
 
   count = 0;
 
+  textPossibilities = new FrequencyList<string>();
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   tryConsume(_node: Node<ts.Node>): boolean {
     this.count += 1;
+    this.textPossibilities.add(_node.getText());
+    console.log(_node.getKind());
     return true;
   }
 
   getFrequencies() {
-    return {};
+    return {
+      text: this.textPossibilities.all,
+    };
   }
 
   getMostCommon() {
     return {
       kind: this.kind,
+      text: this.textPossibilities.mostCommon.value,
     };
   }
 }
